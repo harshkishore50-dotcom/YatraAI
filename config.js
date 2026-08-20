e91928d1dccfa1844aacec9af12aa275c68ba569
Kishore Harsh <harshkishore50@gmail.com>
2026-08-20 21:42:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.YATRA_CONFIG = {
-  AI_ENDPOINT: "YOUR_ACTUAL_VERCEL_URL/api/yatraai",
+  AI_ENDPOINT: "https://yatra-ai-git-main-synapse-six.vercel.app/api/yatraai",
 
   OVERPASS_ENDPOINTS: [
     "https://overpass-api.de/api/interpreter",
